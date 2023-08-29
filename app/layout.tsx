37bbdfbf8abfdf645200ef74c64bb60ab753a652
Sean Gray <seangray.dev@gmail.com>
2023-08-29 16:58:39 -0400

    add favicon + change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "700"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark flex h-screen flex-col`}>
+      <body className={`${montserrat.className} dark flex h-screen flex-col`}>
         {children}
       </body>
     </html>
